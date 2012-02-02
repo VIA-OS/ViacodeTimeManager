d607f8d53a30f9437570c58caed1e2d92d09db04
SanAlex <aandruschenko@gmail.com>
2012-02-02 13:55:27 +0400

    Add static settings and jquery files

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 # Django settings for ViacodeTimeManager project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -45,43 +46,21 @@ USE_I18N = True
 # calendars according to the current locale
 USE_L10N = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
-
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
+
+STATIC_ROOT = 'D:/My Projects/ViacodeTimeManager/static/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Make this unique, and don't share it with anybody.
@@ -96,6 +75,7 @@ TEMPLATE_LOADERS = (
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.static'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -107,6 +87,7 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'ViacodeTimeManager.urls'
+
 
 TEMPLATE_DIRS = ('templates',)
 
@@ -122,6 +103,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'ViacodeTimeManager.Tasks',
+    'ViacodeTimeManager.Content'
     #'south',
 )
 
--- urls.py
+++ urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls.defaults import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'ViacodeTimeManager.Tasks.views.list_all_tasks'),
     (r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls))
 )
+
+urlpatterns += staticfiles_urlpatterns()
