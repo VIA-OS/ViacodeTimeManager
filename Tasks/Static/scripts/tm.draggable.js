223aee4c2936db5844a3f1c0608e1c23f3d18eb0
SanAlex <aandruschenko@gmail.com>
2012-02-13 09:50:08 +0400

    Document ready wrapper

--- Tasks/Static/scripts/tm.draggable.js
+++ Tasks/Static/scripts/tm.draggable.js
@@ -5,13 +5,16 @@
  * Time: 9:37
  * To change this template use File | Settings | File Templates.
  */
-$(function() {
-$( "#draggable" ).draggable({
-    //delay: 1000
-    //distance: 20
-    //snap: true
-    revert: true,
-    helper: "clone",
-    handle: "h3"
-});
-});
+(function() {
+    $(document).ready(function() {
+        $( ".mydrags" ).draggable({
+            //delay: 1000
+            //distance: 20
+            //snap: true
+            revert: true,
+            helper: "clone",
+            handle: "h3"
+        });
+
+    })
+})();
